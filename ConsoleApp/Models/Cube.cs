d333c806a4dfc235ed2ac6f43a6df6ae0de21c87
bobazzia <bobazzia@yahoo.com>
2020-01-12 17:39:03 +0200

    Create all the solution with Models/Cube.cs and behavior in StartUp.cs.

---
+++ ConsoleApp/Models/Cube.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp.Models
+{
+    public class Cube
+    {
+        public int TopNumber { get; set; }
+        public int[] FrontLineNumbers { get; set; }
+        public int BottomNumber { get; set; }
+
+        public int TurnLeft(int currentNumber)
+        {
+            var currentIndex = FindIndex(currentNumber, this.FrontLineNumbers);
+
+            if (currentIndex == this.FrontLineNumbers.Length - 1)
+            {
+                return 0;
+            }
+            else
+            {
+                return currentIndex + 1;
+            }
+        }
+
+        public int TurnRight(int currentNumber)
+        {
+            var currentIndex = FindIndex(currentNumber, this.FrontLineNumbers);
+
+            if (currentIndex == 0)
+            {
+                return this.FrontLineNumbers.Length - 1;
+            }
+            else
+            {
+                return currentIndex - 1;
+            }
+        }
+
+        public int TurnUp(int currentNumber)
+        {
+            if (this.FrontLineNumbers.Contains(currentNumber))
+            {
+                return this.BottomNumber;
+            }
+            else
+            {
+                return this.FrontLineNumbers[0];
+            }
+        }
+
+        public int TurnDown(int currentNumber)
+        {
+            
+            if (this.FrontLineNumbers.Contains(currentNumber))
+            {
+                return this.TopNumber;
+            }
+            else
+            {
+                return this.FrontLineNumbers[0];
+            }
+        }
+
+        public int RotateNumberSixOrNine(int currentNumber)
+        {
+            if (currentNumber == 6)
+            {
+                return 9;
+            }
+
+            if (currentNumber == 9)
+            {
+                return 6;
+            }
+            return 0;
+        }
+
+        public static int FindIndex(int currentNumber, int[] intArray)
+        {
+            var currentNumberIndex = -1;
+            if (intArray.Contains(currentNumber))
+            {
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    if (intArray[i] == currentNumber)
+                    {
+                        currentNumberIndex = i;
+                    }
+                }
+            }
+            return currentNumberIndex;
+        }
+    }
+}
---
+++ ConsoleApp/StartUp.cs
@@ -0,0 +1,172 @@
+﻿using ConsoleApp.Models;
+using System;
+
+namespace ConsoleApp
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("***** Two cubes calendar! Enjoy! *****");
+            Console.WriteLine();
+            Console.WriteLine("This is a two cubes calendar. Every cube have six digits and you can rotate the cubes and set the date.");
+            Console.WriteLine("The command should be in two words and rotate one of the cubes.");
+            Console.WriteLine("The first word must be \"left\" or \"right\" to show which cube you want to rotate");
+            Console.WriteLine("The second can be: \"left\", \"right\", \"up\", \"donw\" or \"rotate\"(you guess for what :) ) for the direction of rotation of the cube");
+            Console.WriteLine("For example: \"left left\", \"right left\", \"right up\", \"left rotate\"");
+            Console.WriteLine("You can rotate the cubes(the left to become right) with command: \"rotate cubes\"");
+            Console.WriteLine();
+            Console.WriteLine("The numbers for the left cube at the start are: 0, 1, 2, 3, 4, 5");
+            Console.WriteLine("The numbers for the right cube at the start are: 0, 1, 2, 6, 7, 8");
+            Console.WriteLine();
+           
+
+            var cubeA = new Cube
+            {
+                BottomNumber = 0,
+                TopNumber = 5,
+                FrontLineNumbers = new int[4]
+                {
+                    1, 2, 3, 4
+                }
+            };
+
+            var cubeB = new Cube
+            {
+                BottomNumber = 0,
+                TopNumber = 8,
+                FrontLineNumbers = new int[4]
+                {
+                    1, 2, 6, 7
+                }
+            };
+
+            var currentLeftNumber = 1;
+            var currentRightNumber = 1;
+            Console.WriteLine("And the start value of the cubes is:");
+            Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+           
+            var input = Console.ReadLine();
+
+            while (input.ToLower() != "end")
+            {
+                var inputParams = input.Split(new char[] { ' ' },
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputParams[0].ToLower() == "left")
+                {
+                    if (inputParams[1].ToLower() == "left")
+                    {
+                        var newIndex = cubeA.TurnLeft(currentLeftNumber);
+                        currentLeftNumber = cubeA.FrontLineNumbers[newIndex];
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "right")
+                    {
+                        var newIndex = cubeA.TurnRight(currentLeftNumber);
+                        currentLeftNumber = cubeA.FrontLineNumbers[newIndex];
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "up")
+                    {
+                        currentLeftNumber = cubeA.TurnUp(currentLeftNumber);
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "down")
+                    {
+                        currentLeftNumber = cubeA.TurnDown(currentLeftNumber);
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "rotate")
+                    {
+                        if (currentLeftNumber == 6 || currentLeftNumber == 9)
+                        {
+                            currentLeftNumber = cubeA.RotateNumberSixOrNine(currentLeftNumber);
+                            Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                        }
+                        
+                        else
+                        {
+                            Console.WriteLine("You can rotate only the numbers 6 and 9.");
+                        }
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Invalid command!");
+                    }
+                }
+                else if(inputParams[0].ToLower() == "right")
+                {
+                    if (inputParams[1].ToLower() == "left")
+                    {
+                        var newIndex = cubeB.TurnLeft(currentRightNumber);
+                        currentRightNumber = cubeB.FrontLineNumbers[newIndex];
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "right")
+                    {
+                        var newIndex = cubeB.TurnRight(currentRightNumber);
+                        currentRightNumber = cubeB.FrontLineNumbers[newIndex];
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "up")
+                    {
+                        currentRightNumber = cubeB.TurnUp(currentRightNumber);
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "down")
+                    {
+                        currentRightNumber = cubeB.TurnDown(currentRightNumber);
+                        Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                    }
+
+                    else if (inputParams[1].ToLower() == "rotate")
+                    {
+                        if (currentRightNumber == 6 || currentRightNumber == 9)
+                        {
+                            currentRightNumber = cubeB.RotateNumberSixOrNine(currentRightNumber);
+
+                            Console.WriteLine("   " + currentLeftNumber + " " + currentRightNumber);
+                        }
+                        
+                        else
+                        {
+                            Console.WriteLine("You can rotate only the numbers 6 and 9.");
+                        }
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("Invalid command!");
+                    }
+                }
+                
+                else if (inputParams[0].ToLower() == "rotate" && inputParams[1].ToLower() == "cubes")
+                {
+                    var currentCube = cubeA;
+                    cubeA = cubeB;
+                    cubeB = currentCube;
+                    currentLeftNumber = 1;
+                    currentRightNumber = 1;
+                    Console.WriteLine("The cubes are rotated.");
+                    Console.WriteLine(currentLeftNumber + " " + currentRightNumber);
+                }
+                
+                else
+                {
+                    Console.WriteLine("Invalid command!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
